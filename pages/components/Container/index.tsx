54165492bf877f74a0e94e398a6e5c87d14d65c3
abraaodev <abraaohomualdo@gmail.com>
2023-03-31 21:45:02 -0300

    remove readm

---
+++ pages/components/Container/index.tsx
@@ -0,0 +1,12 @@
+import { Content } from "./styles";
+
+export function Container(){
+    return(
+        <> 
+
+            <Content>
+                <h1>olá exemplo de componente utilizado 1+ vezes</h1>
+            </Content>
+        </>
+    )
+}
